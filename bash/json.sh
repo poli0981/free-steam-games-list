48d4be04dde3c8a3d34350c40702c79dbcf58b74
poli0981 <127664709+poli0981@users.noreply.github.com>
2026-01-02 06:04:36 +0700

    update git configuration and enhance JSON commit message

--- bash/json.sh
+++ bash/json.sh
@@ -10,6 +10,10 @@ python scripts/update_data.py
 ls -la scripts/
 
 # Commit changes if any
+git config --global user.name "poli0981"
+git config --global user.email "127664709+poli0981@users.noreply.github.com"
+git status
+git add .
 if git diff --cached --quiet; then
     echo "No diff detected"
     git commit --allow-empty -m "JSON updated (timestamp update) [$(date +'%Y-%m-%d %H:%M')]" || echo "Empty commit
