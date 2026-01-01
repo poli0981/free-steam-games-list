cd2a7152bb662562d2e48ed63f1384c9abd4f947
poli0981 <127664709+poli0981@users.noreply.github.com>
2026-01-02 05:08:07 +0700

    Add online script for automated updates and refactor CI workflow

---
+++ bash/online.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+# install dependencies
+pip install requests tqdm
+
+# run the online script
+python scripts/top_online.py
+
+# check file updated
+ls -la
+ls -la games/ || echo "games folder empty bro :("
+ls -la scripts/
+
+# Commit changes
+git config --global user.name "poli0981"
+git config --global user.email "127664709+poli0981@users.noreply.github.com"
+git status
+git add games/top-online.md
+git diff --staged --quiet && echo "No changes – skip commit" || (git commit -m "Auto update tables online top & stats [$(date +'%Y-%m-%d')]" && git pull --rebase && git push)
