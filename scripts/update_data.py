b409b6954418947df472adb2666776052cc6af05
poli0981 <127664709+poli0981@users.noreply.github.com>
2026-01-02 05:53:37 +0700

    fix path again

--- scripts/update_data.py
+++ scripts/update_data.py
@@ -7,13 +7,13 @@ sys.path.append(os.path.dirname(os.path.abspath(__file__)))
 from steam_fetcher import update_all
 
 # Read data.json (folder scripts/)
-with open('data.json', 'r', encoding='utf-8') as f:
+with open('scripts/data.json', 'r', encoding='utf-8') as f:
     games = json.load(f)
 
 update = update_all(games)
 
 # Save data.json fresh
-with open("data.json", "w", encoding="utf-8") as f:
+with open("scripts/data.json", "w", encoding="utf-8") as f:
     json.dump(games, f, indent=4, ensure_ascii=False)
 
 print("Done. JSON file is updated.")
