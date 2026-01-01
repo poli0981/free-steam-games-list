0e1b94ff663391ab938011bf4618815c08c31fca
poli0981 <127664709+poli0981@users.noreply.github.com>
2026-01-02 06:10:19 +0700

    fix: streamline commit process and update echo messages

--- bash/daily.sh
+++ bash/daily.sh
@@ -13,10 +13,4 @@ git config --global user.name "poli0981"
 git config --global user.email "127664709+poli0981@users.noreply.github.com"
 git add games/*.md
 git status
-if git diff --cached --quiet; then
-    echo "No diff detected"
-    git commit --allow-empty -m "Auto regenerate tables (timestamp update) [$(date +'%Y-%m-%d %H:%M')]" || echo "Empty commit skip"
-else
-    git commit -m "Auto regenerate tables from data.json [$(date +'%Y-%m-%d %H:%M')]"
-fi
-    git push || echo "Push fail – check token/permission bro"
+git diff --staged --quiet && echo "No changes – skip commit" || (git commit -m "Auto update tables online top & stats [$(date +'%Y-%m-%d')]" && git push)
