5b63cffea3a03436da8dd2778d1d85bea3dbe22f
poli0981 <127664709+poli0981@users.noreply.github.com>
2026-01-06 01:34:25 +0700

    Update Git configuration for GitHub Actions

--- bash/json.sh
+++ bash/json.sh
@@ -10,14 +10,8 @@ python scripts/update_data.py
 ls -la scripts/
 
 # Commit changes if any
-git config --global user.name "poli0981"
-git config --global user.email "127664709+poli0981@users.noreply.github.com"
+git config --global user.name 'github-actions[bot]'
+git config --global user.email '41898282+github-actions[bot]@users.noreply.github.com'
 git status
 git add .
-if git diff --cached --quiet; then
-    echo "No diff detected"
-    git commit --allow-empty -m "JSON updated (timestamp update) [$(date +'%Y-%m-%d %H:%M')]" || echo "Empty commit skip"
-else
-    git commit -m "JSON updated [$(date +'%Y-%m-%d %H:%M')]"
-fi
-    git push || echo "Push fail – check token/permission bro"
+git diff --staged --quiet && echo "No changes – skip commit" || (git commit -m "Auto update tables online top & stats [$(date +'%Y-%m-%d')] by GitHub Action" && git push)
