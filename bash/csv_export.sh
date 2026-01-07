ace445da910b7c2800cbde3195af3640c4da2468
poli0981 <127664709+poli0981@users.noreply.github.com>
2026-01-07 08:19:15 +0700

    Modify git commit message in csv_export.sh

--- bash/csv_export.sh
+++ bash/csv_export.sh
@@ -2,7 +2,10 @@
 
 pip install pandas
 python scripts/export_data.py
+ls -la
+ls -la/scripts
 git config --global user.name 'github-actions[bot]'
 git config --global user.email '41898282+github-actions[bot]@users.noreply.github.com'
+git status
 git add .
-git diff --staged --quiet && echo "No changes – skip commit" || (git commit -m "Auto update tables online top & stats [$(date +'%Y-%m-%d')] by GitHub Action" && git push)
+git diff --staged --quiet && echo "No changes – skip commit" || (git commit -m "Auto update CSV [$(date +'%Y-%m-%d')] by GitHub Action" && git push)
