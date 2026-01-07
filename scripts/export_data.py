519d59ad26ef260f7444a2f51a8cfd81dfa84d2a
poli0981 <127664709+poli0981@users.noreply.github.com>
2026-01-07 08:17:24 +0700

    Fix CSV export path in export_data.py

--- scripts/export_data.py
+++ scripts/export_data.py
@@ -10,7 +10,7 @@ with open(json_path, "r", encoding="utf-8") as f:
     data = json.load(f)
 
 # Convert to CSV
-pandas.read_json(json_path).to_csv("../data.csv", index=False)
+pandas.read_json(json_path).to_csv("data.csv", index=False)
 
 # If you want to export file with Excel, remove '#' to use.
 # pandas.read_json(json_path).to_excel("../data.xlsx", index=False)
