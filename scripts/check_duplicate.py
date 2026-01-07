c2b0791fa66b421d54ffd16438abbe6eb30facf3
poli0981 <127664709+poli0981@users.noreply.github.com>
2026-01-07 10:05:49 +0700

    Optimize duplicate link detection in check_duplicate.py

--- scripts/check_duplicate.py
+++ scripts/check_duplicate.py
@@ -9,12 +9,15 @@ with open(json_path, 'r', encoding='utf-8') as f:
     data = json.load(f)
 
 # Check for duplicate links
+seen_links = set()
 duplicates = []
-for i in range(len(data)):
-    for j in range(i + 1, len(data)):
-        if data[i]['link'] == data[j]['link']:
-            duplicates.append(data[i]['link'])
-            break
+
+for item in data:
+    link = item['link']
+    if link in seen_links:
+        duplicates.append(link)
+    else:
+        seen_links.add(link)
 
 # Print duplicates
 if duplicates:
